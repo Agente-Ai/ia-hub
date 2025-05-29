4cfd2515f08adf7447ef9631bebac284f402bbba
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-05-29 13:16:59 -0300

    Update driver setup to use standard headless mode and disable GPU for improved compatibility

--- airbnb_scrapper.py
+++ airbnb_scrapper.py
@@ -16,7 +16,7 @@ def __setup_driver():
         options = Options()
 
         options.binary_location = "/usr/bin/google-chrome"
-        options.add_argument("--headless=new")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
