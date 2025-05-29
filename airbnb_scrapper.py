dbb636805cb7ba4ead504c7fbfb8c0760efcc43e
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-05-29 13:05:30 -0300

    Update Chrome driver setup to use local chromedriver and add remote debugging option

--- airbnb_scrapper.py
+++ airbnb_scrapper.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -17,7 +16,7 @@ def __setup_driver():
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
         options.add_argument("--remote-debugging-port=9222")
-        service = Service(ChromeDriverManager().install())
+        service = Service("/usr/bin/chromedriver")
         driver = webdriver.Chrome(service=service, options=options)
         WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.TAG_NAME, "body"))
