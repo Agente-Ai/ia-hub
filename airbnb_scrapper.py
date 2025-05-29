f8736a82fa61881136b0ad555ada2dcd0e0936aa
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-05-29 13:32:31 -0300

    Refactor driver setup to use environment variable for local configuration and improve Chrome options handling

--- airbnb_scrapper.py
+++ airbnb_scrapper.py
@@ -1,32 +1,44 @@
+import os
 import time
 import logging
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+ENV = os.getenv("ENV", "local")
 
 
 def __setup_driver():
     try:
         logger.info("Configurando o driver do Chrome...")
         options = Options()
-        options.binary_location = "/usr/bin/google-chrome"
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--remote-debugging-port=9222")
-        driver = webdriver.Chrome(options=options)
+
+        if ENV == "local":
+            service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=options)
+        else:
+            options.binary_location = "/usr/bin/google-chrome"
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--remote-debugging-port=9222")
+            driver = webdriver.Chrome(options=options)
+
         logger.info("Chrome iniciado. Aguardando carregamento do body...")
+
         WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
-        logger.info("Driver configurado e página carregada.")
+
         return driver
     except Exception as e:
         logger.error(f"❌ Ocorreu um erro ao configurar o driver: {e}")
