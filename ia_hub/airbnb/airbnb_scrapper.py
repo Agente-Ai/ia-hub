ca267c58786c0411457722d7c1809dd38dc534c5
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-05-30 21:31:26 -0300

    Refactor: Update Chrome driver setup to use ChromeDriverManager for installation

--- ia_hub/airbnb/airbnb_scrapper.py
+++ ia_hub/airbnb/airbnb_scrapper.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import (
     TimeoutException,
@@ -63,7 +64,7 @@ def __setup_driver():
                 options.binary_location,
             )
 
-        service = Service("/usr/bin/google-chrome")
+        service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
 
         logger.info("Driver do Chrome configurado com sucesso.")
